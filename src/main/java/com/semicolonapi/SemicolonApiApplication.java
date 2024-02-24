e02279ad97f9346e7389b31c77545fce14fd4046
jodongha <jodongrami@gmail.com>
2024-02-24 15:39:15 +0900

    initial commit

---
+++ src/main/java/com/semicolonapi/SemicolonApiApplication.java
@@ -0,0 +1,13 @@
+package com.semicolonapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SemicolonApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SemicolonApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/semicolonapi/SemicolonApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.semicolonapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SemicolonApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
